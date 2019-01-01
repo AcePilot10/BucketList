c61fd881d2992756dbdbb824f673ef847e5f3120
Cody Gordon <codyjg10@gmail.com>
2019-01-01 04:53:17 -0500

    Continued work on views

---
+++ BucketList.Mobile/BucketList.Mobile/ViewModels/FriendsListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BucketList.Api.Managers;
+using BucketList.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BucketList.Mobile.ViewModels
+{
+    public class FriendsListViewModel
+    {
+        public ObservableCollection<User> Friends { get; set; } = new ObservableCollection<User>();
+
+        public FriendsListViewModel()
+        {
+            InitFriends();    
+        }
+
+        private async void InitFriends()
+        {
+            var user = ((App)Application.Current).User;
+            foreach (string friendId in user.Friends)  
+            {
+                var friend = await UserManager.Instance.GetUsersWhere(x => x.Id == friendId);
+                if (friend[0] != null)
+                {
+                    Friends.Add(friend[0]);
+                }
+            }
+        }
+    }
+}
---
+++ BucketList.Mobile/BucketList.Mobile/ViewModels/NavBarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucketList.Mobile.ViewModels
+{
+    public class NavBarViewModel
+    {
+        public NavBarViewModel()
+        {
+
+        }
+    }
+}
